3a3923ff09afbfffe9b031f8bf8b9e2e308b3297
violent_ken <violent_ken@060f0fd3-4438-4fbc-a035-e2958620a775>
2009-10-01 16:54:39 +0000

    Fixed build date

--- YAPM/Forms/AdditionalMainForms/frmAboutG.Designer.vb
+++ YAPM/Forms/AdditionalMainForms/frmAboutG.Designer.vb
@@ -114,9 +114,9 @@ Partial Class frmAboutG
         Me.lblDate.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblDate.Location = New System.Drawing.Point(155, 63)
         Me.lblDate.Name = "lblDate"
-        Me.lblDate.Size = New System.Drawing.Size(108, 13)
+        Me.lblDate.Size = New System.Drawing.Size(97, 13)
         Me.lblDate.TabIndex = 9
-        Me.lblDate.Text = "Sept. 23 2009 18h18"
+        Me.lblDate.Text = "Oct. 1 2009 15h10"
         '
         'Label4
         '
